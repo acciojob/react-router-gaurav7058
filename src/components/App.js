9f12ce5f2496d20c53aa0d917eb57efeb24703b6
Create_first_repo <gauravchoundiye123@gmail.com>
2024-10-12 16:55:38 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
+import {BrowserRouter,Route,Routes,Link} from "react-router-dom"
+import Home from "./Home";
+import About from "./About";
 
+import Links from "./Links"
 const App = () => {
   return (
+    <BrowserRouter>
+    <Links></Links>
     <div>
-        {/* Do not remove the main div */}
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+        </Routes>
     </div>
+    </BrowserRouter>
   )
 }
 
